332b7005684eabb2c608bf8d6f3692d4170e5cd1
gzhuang <forever_hgz@sina.com>
2024-04-30 14:20:55 +0800

    初始化spring boot learning

---
+++ src/main/java/com/springboot/learning/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package com.springboot.learning.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author forever_hgz
+ */
+@RestController
+public class ConfigController {
+    @Value("${firstName}")
+    private String firstName;
+
+    @Value("${lastName}")
+    private String lastName;
+
+    @GetMapping()
+    public String getName() {
+        return "My name is:" + firstName + lastName;
+    }
+}
