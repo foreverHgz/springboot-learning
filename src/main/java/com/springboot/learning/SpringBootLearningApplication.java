e41465dead5ffc115ab9348fa436e91033fccb4f
gzhuang <forever_hgz@sina.com>
2024-04-30 14:19:49 +0800

    初始化spring boot learning

--- src/main/java/com/springboot/learning/SpringBootLearningApplication.java
+++ src/main/java/com/springboot/learning/SpringBootLearningApplication.java
@@ -1,22 +1,22 @@
 package com.springboot.learning;
 
-import java.util.Date;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * @author forever_hgz
+ */
 @EnableCaching
 @SpringBootApplication
 public class SpringBootLearningApplication {
 
 	public static void main(String[] args) {
-		// http://localhost:8080/redis/swagger-ui/index.html
-		// http://127.0.0.1:8080/redis/swagger-ui/index.html
-		// http://192.168.31.163:8080/redis/swagger-ui/index.html
-		// http://192.168.95.1:8080/redis/swagger-ui/index.html
-		// http://192.168.253.1:8080/redis/swagger-ui/index.html
-		System.out.println("now:" + new Date());
+		// http://localhost:8080/springboot-learning/swagger-ui/index.html
+		// http://127.0.0.1:8080/springboot-learning/swagger-ui/index.html
+		// http://192.168.31.163:8080/springboot-learning/swagger-ui/index.html
+		// http://192.168.95.1:8080/springboot-learning/swagger-ui/index.html
+		// http://192.168.253.1:8080/springboot-learning/swagger-ui/index.html
 		SpringApplication.run(SpringBootLearningApplication.class, args);
 	}
 
--- src/main/java/com/springboot/learning/Swagger2.java
+++ src/main/java/com/springboot/learning/Swagger2.java
@@ -17,10 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-// @Profile("Swagger")
-// @ComponentScan(value = "com.hgz.spring.demo")
 public class Swagger2 {
-
     /**
      * 创建API应用 apiInfo() 增加API相关信息 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
      * 本例采用指定扫描的包路径来定义指定要建立API的目录。
@@ -30,7 +27,7 @@ public class Swagger2 {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-            .apis(RequestHandlerSelectors.basePackage("com.redis.hgz.redis")).paths(PathSelectors.any()).build();
+            .apis(RequestHandlerSelectors.basePackage("com.springboot.learning")).paths(PathSelectors.any()).build();
     }
 
     /**
